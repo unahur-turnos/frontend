1de18288e74cf171201384fb48cc9acd1df8c956
Tiago <tiagocabrera14@gmail.com>
2021-01-19 20:04:15 -0300

    Login centrado

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,18 +2,18 @@ import {
   Box,
   Button,
   Grid,
+  InputAdornment,
   TextField,
   Typography,
-  InputAdornment,
 } from '@material-ui/core';
-
-import { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import IconButton from '@material-ui/core/IconButton';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
-import IconButton from '@material-ui/core/IconButton';
 import { create } from '../../helpers/fetchApi';
+import { useState } from 'react';
 
 export default function Login() {
   const dniRegex = new RegExp('^[0-9]{8}$');
@@ -49,81 +49,82 @@ export default function Login() {
         </Typography>
       </Box>
 
-      <Box display="flex" flexDirection="column" mt={2}>
-        <Box>
-          <Grid
-            container
-            direction="row"
-            justify="space-around"
-            alignItems="flex-end"
-            spacing={3}
-          >
-            <Grid item xs={6}>
-              <Typography variant="h6">Número de documento:</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                id="dni"
-                label="Ingrese su documento"
-                name="dni"
-                onChange={handleChange}
-                validations={{ matchRegexp: dniRegex }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="start">
-                      <AssignmentIndIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                style={{ minWidth: 250 }}
-              />
-            </Grid>
+      <Grid
+        container
+        alignItems="flex-end"
+        spacing={4}
+        style={{ marginTop: '8px' }}
+      >
+        <Grid item xs={6} align="right">
+          <Typography variant="h6">Número de documento:</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="dni"
+            label="Ingrese su documento"
+            name="dni"
+            onChange={handleChange}
+            validations={{ matchRegexp: dniRegex }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <AssignmentIndIcon />
+                </InputAdornment>
+              ),
+            }}
+            style={{ minWidth: 250 }}
+          />
+        </Grid>
 
-            <Grid item xs={6}>
-              <Typography variant="h6">Contraseña:</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                id="contrasenia"
-                label="Ingrese una contraseña"
-                name="contrasenia"
-                type={showPassword ? 'text' : 'password'}
-                onChange={handleChange}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                      >
-                        {showPassword ? (
-                          <VisibilityIcon />
-                        ) : (
-                          <VisibilityOffIcon />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                style={{ minWidth: 250 }}
-              />
-            </Grid>
+        <Grid item xs={6} align="right">
+          <Typography variant="h6">Contraseña:</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="contrasenia"
+            label="Ingrese una contraseña"
+            name="contrasenia"
+            type={showPassword ? 'text' : 'password'}
+            onChange={handleChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                  >
+                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            style={{ minWidth: 250 }}
+          />
+        </Grid>
+
+        <Grid container item xs={12} spacing={1}>
+          <Grid item xs={6} align="right">
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              onClick={validarLogin}
+            >
+              Iniciar sesión
+            </Button>
           </Grid>
-        </Box>
-        <Box mt={7} justifyContent="center" display="flex">
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            onClick={validarLogin}
-          >
-            Iniciar sesión
-          </Button>
-          <Button component={Link} to="/registro">
-            Registrarse
-          </Button>
-        </Box>
-      </Box>
+          <Grid item xs={6} align="left">
+            <Button
+              variant="contained"
+              color="inherit"
+              component={Link}
+              to="/registro"
+            >
+              Registrarse
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
