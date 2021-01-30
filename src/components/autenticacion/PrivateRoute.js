722a0e350dadf25e7c6ed494b5a5890642a580f6
FlorenciaMssy <florenciamssy@gmail.com>
2021-01-30 20:57:11 -0300

    funcionando

--- src/components/autenticacion/PrivateRoute.js
+++ src/components/autenticacion/PrivateRoute.js
@@ -1,16 +1,77 @@
 import { Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { estaAutorizadoState } from '../../state/usuario';
+import { estaAutorizadoState, usuarioState } from '../../state/usuario';
 import { useRecoilValue } from 'recoil';
+
+const rutas = [
+  {
+    nombre: 'Actividades',
+    ruta: '/actividades',
+    roles: ['admin', 'bedel'],
+  },
+  {
+    nombre: 'Crear actividad',
+    ruta: '/actividades/nueva',
+    roles: ['admin', 'bedel'],
+  },
+  {
+    nombre: 'Pedir autorización',
+    ruta: '/autorizaciones/nueva',
+    roles: ['asistente', 'admin'],
+  },
+  {
+    nombre: 'Confirmar autorización',
+    ruta: '/autorizaciones/confirmacion',
+    roles: ['asistente', 'admin'],
+  },
+
+  {
+    nombre: 'Espacio id',
+    ruta: '/espacios/:id',
+    roles: ['admin', 'bedel'],
+  },
+  {
+    nombre: 'Crear espacio',
+    ruta: '/espacios/nuevo',
+    roles: ['admin', 'bedel'],
+  },
+  {
+    nombre: 'Ver espacios',
+    ruta: '/espacios',
+    roles: ['admin', 'bedel'],
+  },
+  {
+    nombre: 'Registro',
+    ruta: '/registro',
+    roles: ['asistente', 'admin', 'bedel'],
+  },
+  {
+    nombre: 'Confirmar autorización',
+    ruta: '/login',
+    roles: ['asistente', 'admin', 'bedel'],
+  },
+];
 
 export default function PrivateRoute({ children, ...rest }) {
   const estaAutorizado = useRecoilValue(estaAutorizadoState);
+  const estadoUsuario = useRecoilValue(usuarioState);
+
+  const validarRutaConRol = () => {
+    const ruta = rutas.find((rutaJSON) => rutaJSON.ruta == rest.path);
+
+    console.log(rutas);
+    return ruta.roles.some((rol) => rol == estadoUsuario?.rol) ? (
+      children
+    ) : (
+      <Redirect to={'/login'} />
+    );
+  };
 
   return (
-    <Route
-      {...rest}
-      render={() => (estaAutorizado ? children : <Redirect to={'/login'} />)}
-    />
+    <>
+      {console.log(rest.path)}
+      <Route {...rest} render={validarRutaConRol} />
+    </>
   );
 }
 
