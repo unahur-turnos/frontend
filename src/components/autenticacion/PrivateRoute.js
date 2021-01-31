b233675cbaafc495e59609fd119e5501d6353da4
Joaquin <joaquinpettinari@hotmail.com>
2021-01-30 22:45:59 -0300

    listo para la review

--- src/components/autenticacion/PrivateRoute.js
+++ src/components/autenticacion/PrivateRoute.js
@@ -1,59 +1,9 @@
 import { Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { estaAutorizadoState, usuarioState } from '../../state/usuario';
+import { usuarioState } from '../../state/usuario';
 import { useRecoilValue } from 'recoil';
 
-const rutas = [
-  {
-    nombre: 'Actividades',
-    ruta: '/actividades',
-    roles: ['admin', 'bedel'],
-  },
-  {
-    nombre: 'Crear actividad',
-    ruta: '/actividades/nueva',
-    roles: ['admin', 'bedel'],
-  },
-  {
-    nombre: 'Pedir autorización',
-    ruta: '/autorizaciones/nueva',
-    roles: ['asistente', 'admin'],
-  },
-  {
-    nombre: 'Confirmar autorización',
-    ruta: '/autorizaciones/confirmacion',
-    roles: ['asistente', 'admin'],
-  },
-
-  {
-    nombre: 'Espacio id',
-    ruta: '/espacios/:id',
-    roles: ['admin', 'bedel'],
-  },
-  {
-    nombre: 'Crear espacio',
-    ruta: '/espacios/nuevo',
-    roles: ['admin', 'bedel'],
-  },
-  {
-    nombre: 'Ver espacios',
-    ruta: '/espacios',
-    roles: ['admin', 'bedel'],
-  },
-  {
-    nombre: 'Registro',
-    ruta: '/registro',
-    roles: ['asistente', 'admin', 'bedel'],
-  },
-  {
-    nombre: 'Confirmar autorización',
-    ruta: '/login',
-    roles: ['asistente', 'admin', 'bedel'],
-  },
-];
-
 export default function PrivateRoute({ children, ...rest }) {
-  const estaAutorizado = useRecoilValue(estaAutorizadoState);
   const estadoUsuario = useRecoilValue(usuarioState);
 
   const validarRutaConRol = () => {
@@ -74,6 +24,42 @@ export default function PrivateRoute({ children, ...rest }) {
     </>
   );
 }
+
+const rutas = [
+  { nombre: 'Actividades', ruta: '/actividades', roles: ['admin', 'bedel'] },
+  {
+    nombre: 'Crear actividad',
+    ruta: '/actividades/nueva',
+    roles: ['admin', 'bedel'],
+  },
+  {
+    nombre: 'Pedir autorización',
+    ruta: '/autorizaciones/nueva',
+    roles: ['asistente', 'admin'],
+  },
+  {
+    nombre: 'Confirmar autorización',
+    ruta: '/autorizaciones/confirmacion',
+    roles: ['asistente', 'admin'],
+  },
+  { nombre: 'Espacio id', ruta: '/espacios/:id', roles: ['admin', 'bedel'] },
+  {
+    nombre: 'Crear espacio',
+    ruta: '/espacios/nuevo',
+    roles: ['admin', 'bedel'],
+  },
+  { nombre: 'Ver espacios', ruta: '/espacios', roles: ['admin', 'bedel'] },
+  {
+    nombre: 'Registro',
+    ruta: '/registro',
+    roles: ['asistente', 'admin', 'bedel'],
+  },
+  {
+    nombre: 'Confirmar autorización',
+    ruta: '/login',
+    roles: ['asistente', 'admin', 'bedel'],
+  },
+];
 
 PrivateRoute.propTypes = {
   isAuthenticated: PropTypes.bool,
