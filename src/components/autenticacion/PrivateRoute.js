0701d623917c336b995536d1f05f49db27d474f6
Joaquin <joaquinpettinari@hotmail.com>
2021-01-31 11:40:51 -0300

    Cambios propuestos por Fede, se pasa por prop los roles permitidos

--- src/components/autenticacion/PrivateRoute.js
+++ src/components/autenticacion/PrivateRoute.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 import { usuarioState } from '../../state/usuario';
 import { useRecoilValue } from 'recoil';
 
-export default function PrivateRoute({ children, ...rest }) {
+export default function PrivateRoute({ children, rolesPermitidos, ...rest }) {
   const estadoUsuario = useRecoilValue(usuarioState);
 
   const validarRutaConRol = () => {
-    const ruta = rutas.find((rutaJSON) => rutaJSON.ruta == rest.path);
-
-    return ruta.roles.some((rol) => rol == estadoUsuario?.rol) ? (
+    return rolesPermitidos.some((rol) => rol == estadoUsuario?.rol) ? (
       children
     ) : (
       <Redirect to={'/login'} />
@@ -23,43 +21,7 @@ export default function PrivateRoute({ children, ...rest }) {
   );
 }
 
-const rutas = [
-  { nombre: 'Actividades', ruta: '/actividades', roles: ['admin', 'bedel'] },
-  {
-    nombre: 'Crear actividad',
-    ruta: '/actividades/nueva',
-    roles: ['admin', 'bedel'],
-  },
-  {
-    nombre: 'Pedir autorización',
-    ruta: '/autorizaciones/nueva',
-    roles: ['asistente', 'admin'],
-  },
-  {
-    nombre: 'Confirmar autorización',
-    ruta: '/autorizaciones/confirmacion',
-    roles: ['asistente', 'admin'],
-  },
-  { nombre: 'Espacio id', ruta: '/espacios/:id', roles: ['admin', 'bedel'] },
-  {
-    nombre: 'Crear espacio',
-    ruta: '/espacios/nuevo',
-    roles: ['admin', 'bedel'],
-  },
-  { nombre: 'Ver espacios', ruta: '/espacios', roles: ['admin', 'bedel'] },
-  {
-    nombre: 'Registro',
-    ruta: '/registro',
-    roles: ['asistente', 'admin', 'bedel'],
-  },
-  {
-    nombre: 'Confirmar autorización',
-    ruta: '/login',
-    roles: ['asistente', 'admin', 'bedel'],
-  },
-];
-
 PrivateRoute.propTypes = {
-  isAuthenticated: PropTypes.bool,
   children: PropTypes.object,
+  rolesPermitidos: PropTypes.object,
 };
