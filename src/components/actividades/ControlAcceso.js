76d197aa76f11382c9b978226fadac283569bd18
Tiago <tiagocabrera14@gmail.com>
2021-01-31 20:33:10 -0300

    Se separa el componente de autocompletado

--- src/components/actividades/ControlAcceso.js
+++ src/components/actividades/ControlAcceso.js
@@ -57,66 +57,73 @@ export default function ControlAcceso() {
   return (
     <>
       <Grid container className={classes.container} spacing={4}>
-        <Grid item xs={12} align="Center">
+        <Grid item xs={12} align="center">
           <Typography variant="h4" color="primary">
             Actividades del día
           </Typography>
         </Grid>
 
         <Grid item xs={12} align="center">
-          <Autocomplete
-            id="actividades"
-            options={actividades}
-            getOptionLabel={(actividad) => actividad.nombre}
-            className={classes.autocomplete}
-            noOptionsText="No hay actividades que coincidan con la búsqueda"
-            onChange={(event, actividad) => {
-              setActividadSeleccionada(actividad);
-            }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Seleccione una actividad"
-                variant="outlined"
-              />
-            )}
-            renderOption={(actividad) => {
-              return (
-                <Grid container alignItems="center">
-                  <Grid item>
-                    <AssignmentIcon color="primary" className={classes.icon} />
-                  </Grid>
-                  <Grid item xs>
-                    <Typography variant="body1" style={{ fontWeight: 700 }}>
-                      {actividad.nombre}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      {`${actividad.Espacio.Edificio.nombre} - ${actividad.Espacio.nombre}`}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              );
-            }}
+          <SeleccionDeActividad
+            actividades={actividades}
+            setActividadSeleccionada={setActividadSeleccionada}
           />
         </Grid>
 
         <Grid item xs={12} align="center">
-          {actividadSeleccionada ? (
+          {actividadSeleccionada && (
             <DatosActividad actividad={actividadSeleccionada} />
-          ) : (
-            ''
           )}
         </Grid>
 
         <Grid item xs={12} align="center">
-          {actividadSeleccionada ? (
+          {actividadSeleccionada && (
             <ListadoAutorizaciones idActividad={actividadSeleccionada.id} />
-          ) : (
-            ''
           )}
         </Grid>
       </Grid>
     </>
+  );
+}
+
+function SeleccionDeActividad({ actividades, setActividadSeleccionada }) {
+  const classes = useStyles();
+
+  return (
+    <Autocomplete
+      id="actividades"
+      options={actividades}
+      getOptionLabel={(actividad) => actividad.nombre}
+      className={classes.autocomplete}
+      noOptionsText="No hay actividades que coincidan con la búsqueda"
+      onChange={(event, actividad) => {
+        setActividadSeleccionada(actividad);
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Seleccione una actividad"
+          variant="outlined"
+        />
+      )}
+      renderOption={(actividad) => {
+        return (
+          <Grid container alignItems="center">
+            <Grid item>
+              <AssignmentIcon color="primary" className={classes.icon} />
+            </Grid>
+            <Grid item xs>
+              <Typography variant="body1" style={{ fontWeight: 700 }}>
+                {actividad.nombre}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                {`${actividad.Espacio.Edificio.nombre} - ${actividad.Espacio.nombre}`}
+              </Typography>
+            </Grid>
+          </Grid>
+        );
+      }}
+    />
   );
 }
 
@@ -180,6 +187,11 @@ function ListadoAutorizaciones({ idActividad }) {
     </TableContainer>
   );
 }
+
+SeleccionDeActividad.propTypes = {
+  actividades: PropTypes.arrayOf(PropTypes.object),
+  setActividadSeleccionada: PropTypes.func,
+};
 
 DatosActividad.propTypes = {
   actividad: PropTypes.object,
