e597cb2be63c6b53086681c7f524729308670e74
Joaquin <joaquinpettinari@hotmail.com>
2021-02-11 19:53:50 -0300

    Falta modificar las cosas

--- src/components/inscripcionAutorizacion/Paso1DDJJ.js
+++ src/components/inscripcionAutorizacion/Paso1DDJJ.js
@@ -8,6 +8,8 @@ import {
   TextField,
   Tooltip,
   Typography,
+  Button,
+  makeStyles,
 } from '@material-ui/core';
 
 import Autocomplete from '@material-ui/lab/Autocomplete';
@@ -18,11 +20,10 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useRecoilValue } from 'recoil';
 import HelpIcon from '@material-ui/icons/Help';
 import { AYUDAS } from '../textos/Textos';
-import theme from '../../theme';
+import { useState } from 'react';
 
 export default function Paso1DDJJ({ handleChange, agregarUnValor }) {
-  const matches = useMediaQuery('(min-width:800px)');
-  const { it } = theme.breakpoints.values;
+  const matches = useMediaQuery('(min-width:960px)');
   const actividades = useRecoilValue(todasLasActividades());
 
   const cambioDeActividad = (nombre, actividad) => {
@@ -32,11 +33,11 @@ export default function Paso1DDJJ({ handleChange, agregarUnValor }) {
   return (
     <>
       <Grid container alignItems="flex-end" spacing={4}>
-        <Grid item xs={12} it={6} md={6} align={matches ? 'right' : 'center'}>
+        <Grid item xs={12} md={6} align={matches ? 'right' : 'center'}>
           <Typography variant="h6">Seleccioná actividad:</Typography>
         </Grid>
 
-        <Grid item xs={12} it={6} align={!matches && 'center'}>
+        <Grid item xs={12} md={6} align={!matches && 'center'}>
           <FormControl style={{ minWidth: 250 }}>
             <Autocomplete
               options={actividades}
@@ -69,19 +70,21 @@ export default function Paso1DDJJ({ handleChange, agregarUnValor }) {
               )}
             />
           </FormControl>
-          {
+          {/* {matches && (
             <ToolTipButton
               mensaje={AYUDAS.autorizacionSelectorActividades}
               margen={25}
             />
-          }
+            // <Popover mensaje={AYUDAS.autorizacionSelectorActividades} />
+          )} */}
+          {/* <Popover /> */}
         </Grid>
 
-        <Grid item xs={12} it={6} align={matches ? 'right' : 'center'}>
+        <Grid item xs={12} md={6} align={matches ? 'right' : 'center'}>
           <Typography variant="h6">Medio de transporte:</Typography>
         </Grid>
 
-        <Grid item xs={12} it={6} align={!matches && 'center'}>
+        <Grid item xs={12} md={6} align={!matches && 'center'}>
           <FormControl style={{ minWidth: 250 }}>
             <InputLabel id="medioDeTransporte">Elija un transporte</InputLabel>
             <Select
@@ -121,6 +124,59 @@ function ToolTipButton({ mensaje, margen }) {
   );
 }
 
+function Popover({ mensaje }) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <div>
+      <Button
+        aria-describedby={id}
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        Open Popover
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>
+          The content of the Popover.
+        </Typography>
+      </Popover>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  typography: {
+    padding: theme.spacing(2),
+  },
+}));
+
 Paso1DDJJ.propTypes = {
   handleChange: PropTypes.func,
   agregarUnValor: PropTypes.func,
@@ -129,4 +185,8 @@ Paso1DDJJ.propTypes = {
 ToolTipButton.propTypes = {
   mensaje: PropTypes.string,
   margen: PropTypes.integer,
+};
+
+Popover.propTypes = {
+  mensaje: PropTypes.string,
 };
--- src/theme.js
+++ src/theme.js
@@ -20,16 +20,6 @@ const theme = createMuiTheme({
       '"Segoe UI Symbol"',
     ].join(','),
   },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      it: 800,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
-    },
-  },
 });
 
 export default theme;
