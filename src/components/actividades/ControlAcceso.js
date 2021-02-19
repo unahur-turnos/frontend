21d9aa83b0d71ecde7fb571baddb93a59bd06ccb
Federico Aloi <federico.aloi@gmail.com>
2021-02-19 12:51:11 -0300

    Algunos ajustes estéticos

--- src/components/actividades/ControlAcceso.js
+++ src/components/actividades/ControlAcceso.js
@@ -22,7 +22,7 @@ import { DateTime } from 'luxon';
 import { PropTypes } from 'prop-types';
 import SelectorActividad from './SelectorActividad';
 import { autorizacionesPorActividad } from '../../state/autorizaciones';
-import { hourFormatter } from '../../utils/dateUtils';
+import { hourFormatter, fechaHoraActividad } from '../../utils/dateUtils';
 import { compose, filter, isNil, sortWith, ascend, propOr } from 'ramda';
 import { todasLasActividades } from '../../state/actividades';
 import { useRecoilValue } from 'recoil';
@@ -44,7 +44,6 @@ const useStyles = makeStyles((theme) => ({
   },
   card: {
     maxWidth: 320,
-    borderRadius: 20,
     borderColor: theme.palette.primary.main,
   },
   table: {
@@ -66,15 +65,14 @@ export default function ControlAcceso() {
   const [actividadSeleccionada, setActividadSeleccionada] = useState(null);
 
   return (
-    <>
-      <Grid container className={classes.container} spacing={4}>
-        <Grid item xs={12} align="center">
-          <Typography variant="h4" color="primary">
-            Actividades del día
-          </Typography>
-        </Grid>
-
-        <Grid item xs={12} align="center">
+    <Grid container className={classes.container} spacing={4}>
+      <Grid item md={12} align="center">
+        <Typography variant="h4" color="primary">
+          Control de acceso
+        </Typography>
+      </Grid>
+      <Grid container justify="center" spacing={4}>
+        <Grid item>
           <SelectorActividad
             actividades={actividades}
             funcionOnChange={setActividadSeleccionada}
@@ -82,19 +80,18 @@ export default function ControlAcceso() {
           />
         </Grid>
 
-        <Grid item xs={12} align="center">
+        <Grid item>
           {actividadSeleccionada && (
             <DatosActividad actividad={actividadSeleccionada} />
           )}
         </Grid>
-
-        <Grid item xs={12} align="center">
-          {actividadSeleccionada && (
-            <ListadoAutorizaciones idActividad={actividadSeleccionada.id} />
-          )}
-        </Grid>
       </Grid>
-    </>
+      <Grid item md={12} align="center">
+        {actividadSeleccionada && (
+          <ListadoAutorizaciones idActividad={actividadSeleccionada.id} />
+        )}
+      </Grid>
+    </Grid>
   );
 }
 
@@ -106,10 +103,10 @@ function DatosActividad({ actividad }) {
     <Card className={classes.card} variant="outlined">
       <CardContent>
         <Typography variant="body1">{`${Espacio.nombre} (${Espacio.Edificio.nombre})`}</Typography>
-        <Typography variant="body1">
-          {`${hourFormatter(fechaHoraInicio)} - ${hourFormatter(fechaHoraFin)}`}
+        <Typography variant="body2">
+          {fechaHoraActividad(fechaHoraInicio, fechaHoraFin)}
         </Typography>
-        <Typography variant="body1">{`Responsable: ${responsable}`}</Typography>
+        <Typography variant="body2">{`Responsable: ${responsable}`}</Typography>
       </CardContent>
     </Card>
   );
@@ -125,7 +122,7 @@ function ListadoAutorizaciones({ idActividad }) {
   const [autorizacionARegistrar, setAutorizacionARegistrar] = useState();
 
   const [abrirModal, setAbrirModal] = useState(false);
-  const [mostrarRegistrados, setMostrarRegistrados] = useState(true);
+  const [ocultarRegistrados, setOcultarRegistrados] = useState(false);
 
   // El useMemo evita que esto se recalcule a cada rato, solo lo hace si cambian sus dependencias.
   const autorizacionesFiltradas = useMemo(
@@ -135,9 +132,9 @@ function ListadoAutorizaciones({ idActividad }) {
           ascend(propOr(minDate, 'fechaHoraIngreso')),
           ascend(path(['Usuario', 'apellido'])),
         ]),
-        filter((it) => mostrarRegistrados || isNil(it.fechaHoraIngreso))
+        filter((it) => !ocultarRegistrados || isNil(it.fechaHoraIngreso))
       )(todasLasAutorizaciones),
-    [todasLasAutorizaciones, mostrarRegistrados]
+    [todasLasAutorizaciones, ocultarRegistrados]
   );
 
   const confirmarRegistro = (autorizacion) => {
@@ -145,17 +142,15 @@ function ListadoAutorizaciones({ idActividad }) {
     setAbrirModal(true);
   };
 
-  const cambioCheck = () => setMostrarRegistrados(!mostrarRegistrados);
+  const cambioCheck = () => setOcultarRegistrados(!ocultarRegistrados);
 
   return (
     <>
       <Box display="flex" justifyContent="center">
         <FormGroup>
           <FormControlLabel
-            label="Mostrar asistentes ya registrados"
-            control={
-              <Switch defaultChecked onChange={cambioCheck} color="primary" />
-            }
+            label="Ocultar registrados"
+            control={<Switch onChange={cambioCheck} color="primary" />}
           />
         </FormGroup>
       </Box>
