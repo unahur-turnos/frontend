202bc4d779d48cd0eaa5d6d2bb5b52033e0b14b9
Joaquin <joaquinpettinari@hotmail.com>
2021-02-18 23:45:40 -0300

    Adding Accordion, its working but the style is sheet

--- src/components/misActividades/MisActividades.js
+++ src/components/misActividades/MisActividades.js
@@ -8,15 +8,25 @@ import {
   Typography,
   TableBody,
   TableCell,
+  Fab,
+  makeStyles,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Divider,
+  AccordionActions,
 } from '@material-ui/core';
 import { useRecoilValue } from 'recoil';
 import { PropTypes } from 'prop-types';
 import { todasLasAutorizacionesState } from '../../state/usuario';
 import { DateTime } from 'luxon';
+import AddIcon from '@material-ui/icons/Add';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Link } from 'react-router-dom';
 
 export default function MisActividades() {
   const autorizaciones = useRecoilValue(todasLasAutorizacionesState);
+  const classes = useStyles();
 
   const autorizacionesFuturas = (futuro) =>
     autorizaciones.filter((autorizacion) =>
@@ -29,51 +39,78 @@ export default function MisActividades() {
 
   return (
     <>
+      <Grid style={{ width: '100%', height: '100%' }}>
+        <Fab color="primary" aria-label="add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </Grid>
       <Grid container spacing={4}>
         <Grid item xs={12} align="center">
           <Typography variant="h4" color="primary">
             Mis actividades
           </Typography>
         </Grid>
-        <Grid container xs={12}>
-          <Grid item xs={12} sm={8} md={10}>
-            <Typography variant="h6">Actividades futuras</Typography>
-          </Grid>
-          <Grid item xs={12} sm={4} md={2}>
-            <Button
-              variant="contained"
-              color="primary"
-              component={Link}
-              to="/autorizaciones/nueva"
-            >
-              Solicitar actividad
-            </Button>
-          </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">Futuras actividades</Typography>
         </Grid>
-        <Tabla data={autorizacionesFuturas(true)} />
+        <Acordion data={autorizacionesFuturas(true)} />
         <Grid item xs={12}>
           <Typography variant="h6">Actividades pasadas</Typography>
         </Grid>
-        <Tabla data={autorizacionesFuturas(false)} />
+        <Acordion data={autorizacionesFuturas(false)} />
       </Grid>
     </>
   );
 }
 
-function Tabla(props) {
+function Acordion(props) {
   const { data } = props;
+  const classes = useStyles();
+
   return (
     <>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Nombre actividad</TableCell>
-              <TableCell>Responsable</TableCell>
-              <TableCell>Hora ingreso</TableCell>
-              <TableCell>Hora final</TableCell>
-            </TableRow>
-          </TableHead>
+      <Divider />
+      {data.map((autorizacion) => (
+        <Grid container key={autorizacion.id}>
+          <Grid item xs={12}>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <div className={classes.column}>
+                  <Typography className={classes.heading}>
+                    {autorizacion.Actividad.nombre}{' '}
+                  </Typography>
+                </div>
+                <div className={classes.column}>
+                  <Typography className={classes.secondaryHeading}>
+                    {`${DateTime.fromISO(autorizacion.Actividad.fechaHoraInicio)
+                      .setLocale('es')
+                      .toFormat("D 'de' t 'a'")}
+                    ${DateTime.fromISO(autorizacion.Actividad.fechaHoraFin)
+                      .setLocale('es')
+                      .toFormat('t')}
+                    `}
+                  </Typography>
+                </div>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className={classes.column}>
+                  <Typography className={classes.heading}>
+                    Responsable: {autorizacion.Actividad.responsable}
+                  </Typography>
+                </div>
+              </AccordionDetails>
+              <Divider />
+              <AccordionActions>
+                <Button size="medium" variant="contained" color="secondary">
+                  Cancelar actividad
+                </Button>
+              </AccordionActions>
+            </Accordion>
+          </Grid>
+        </Grid>
+      ))}
+      {/* <TableContainer>
+        <Table>          
           <TableBody>
             {data.map((autorizacion) => (
               <TableRow key={autorizacion.id}>
@@ -93,11 +130,29 @@ function Tabla(props) {
             ))}
           </TableBody>
         </Table>
-      </TableContainer>
+      </TableContainer> */}
     </>
   );
 }
 
-Tabla.propTypes = {
+Acordion.propTypes = {
   data: PropTypes.obj,
 };
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  column: {
+    flexBasis: '33.33%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
