6752007cd96248a06fcd9b55baefb4ba24843314
Federico Aloi <federico.aloi@gmail.com>
2021-02-25 09:48:17 -0300

    Ajustes de CSS BotonGuardar

--- src/components/ui/BotonGuardar.js
+++ src/components/ui/BotonGuardar.js
@@ -1,9 +1,7 @@
-import { CircularProgress, makeStyles, Button } from '@material-ui/core';
+import { CircularProgress, Button } from '@material-ui/core';
 import { PropTypes } from 'prop-types';
 
 export function BotonGuardar({ loading, texto = 'Guardar' }) {
-  const classes = useStyles();
-
   return (
     <Button
       variant="contained"
@@ -11,20 +9,10 @@ export function BotonGuardar({ loading, texto = 'Guardar' }) {
       type="submit"
       disabled={loading}
     >
-      {loading ? (
-        <CircularProgress color="white" className={classes.loading} size={25} />
-      ) : (
-        texto
-      )}
+      {loading ? <CircularProgress size={25} /> : texto}
     </Button>
   );
 }
-
-const useStyles = makeStyles(() => ({
-  loading: {
-    marginRight: '10px',
-  },
-}));
 
 BotonGuardar.propTypes = {
   loading: PropTypes.bool,
