5d57a3dfa642ce9eb326a49f9e54d93a52216725
Joaquin <joaquinpettinari@hotmail.com>
2021-02-28 21:17:40 -0300

    Se modifica un poco la pantalla, faltan muchas cosas

--- src/components/login/RecuperarContrasenia.js
+++ src/components/login/RecuperarContrasenia.js
@@ -1,8 +1,39 @@
-import { Card, Typography, Grid, CardContent, Box } from '@material-ui/core';
-import { Alert } from '@material-ui/lab';
-import { TextValidator } from 'react-material-ui-form-validator';
+import {
+  Card,
+  Typography,
+  Grid,
+  CardContent,
+  Box,
+  InputAdornment,
+  Button,
+} from '@material-ui/core';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator';
+import { ERRORES } from '../textos/Textos';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import { BotonGuardar } from '../ui/BotonGuardar';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useInputStyles } from '../../utils/numberFieldWithoutArrows';
 
 export default function RecuperarContrasenia() {
+  const [iconoCargando, setIconoCargando] = useState(false);
+  const [dni, setDNI] = useState();
+  const history = useHistory();
+  const inputClasses = useInputStyles();
+
+  const mandarMail = () => {
+    setIconoCargando(true);
+  };
+
+  const handleChange = (e) => {
+    setDNI(e.target.value);
+  };
+
+  const volverAlLogin = () => {
+    history.push('/login');
+  };
+
   return (
     <>
       <Box mt={5} display="flex" justifyContent="center">
@@ -10,13 +41,55 @@ export default function RecuperarContrasenia() {
           Recuperar contraseña
         </Typography>
       </Box>
-      <Grid container spacing={4} align="center">
-        <Grid item xs={12}>
-          <Grid item xs={9} sm={7} md={4} style={{ marginTop: 20 }}>
-            Hola
+      <ValidatorForm onSubmit={mandarMail}>
+        <Grid container spacing={4} align="center">
+          <Grid item xs={12}>
+            <Grid item xs={9} sm={7} md={4} style={{ marginTop: 20 }}>
+              <Alert severity="info" align="left">
+                <AlertTitle>¿Olvidaste tu contraseña?</AlertTitle>
+                <br />
+                <Typography variant="body2">
+                  Para recibir tu código de acceso por correo electrónico,
+                  ingresá tu DNI
+                </Typography>
+              </Alert>
+              <Grid item xs={12} style={{ marginTop: 20 }}>
+                <TextValidator
+                  id="dni"
+                  label="Ingresá tu documento"
+                  onChange={handleChange}
+                  name="dni"
+                  type="number"
+                  className={inputClasses.numberFieldWithoutArrows}
+                  fullWidth
+                  value={dni}
+                  validators={[
+                    'required',
+                    'minNumber:1000000',
+                    'maxNumber:99999999',
+                  ]}
+                  errorMessages={[ERRORES.requerido, ERRORES.dni, ERRORES.dni]}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="start">
+                        <AssignmentIndIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid container style={{ marginTop: 20 }}>
+                <Grid item xs={6} align="left">
+                  <Button onClick={volverAlLogin}>Volver al login</Button>
+                </Grid>
+                <Grid item xs={6} align="right">
+                  <BotonGuardar loading={iconoCargando} texto="Mandar mail" />
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </ValidatorForm>
     </>
   );
 }
