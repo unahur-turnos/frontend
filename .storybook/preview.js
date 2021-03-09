82dfe68cd55694869c9fa27ae9da55910d49e38b
Federico Aloi <federico.aloi@gmail.com>
2021-03-09 18:44:45 -0300

    Decorator con el tema de Material

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import theme from '../src/theme';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Story />
+      </CssBaseline>
+    </ThemeProvider>
+  ),
+];
